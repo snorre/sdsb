f178d9385e69bf39a334bf3eb916dd4528cc0180
Snorre Visnes <snorre.visnes@gmail.com>
2017-10-21 22:05:22 +0200

    Oops.

--- sdsb_config.sh
+++
@@ -1,7 +0,0 @@
-local_data_root="/home/snorre/dev/sdsb/testdata"
-remote_server="bm-dev.entotre.org"
-remote_data_root="/data/backup_dev/data"
-remote_snapshot_root="/data/backup_dev/snapshots"
-bandwidth_limit_KBs=1000
-sendgrid_api_key="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"
-notification_email="snorre.visnes@gmail.com"
