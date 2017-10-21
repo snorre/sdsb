7fd438fbf781f1788cf78375ce78e253fe6d3faf
Snorre Visnes <snorre.visnes@gmail.com>
2017-10-21 22:42:20 +0200

    Use fd3 to send log

--- sdsb_cron.sh
+++ sdsb_cron.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
+exec 3<<<"" # Create a deleted temporary fil: /dev/fd/3
+
 . sdsb_lib.sh
 
 read_config
 
-bash sdsb_run.sh &> sdsb.log
+bash sdsb_run.sh 2>&1 | tee /dev/fd/3
 
-send_notification sdsb.log
-
-#rm sdsb.log
+send_notification /dev/fd/3
